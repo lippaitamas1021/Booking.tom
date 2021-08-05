f7d06d86b18eaeecddabf7ccdb773eb424bb00f0
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-08-05 11:20:55 +0200

    Initial commit - configuration files

---
+++ src/main/resources/db/migration/V1__rooms.sql
@@ -0,0 +1,4 @@
+CREATE TABLE rooms (id bigint(10) AUTO_INCREMENT,
+roomNumber varchar(255),
+guests varchar(255),
+PRIMARY KEY (id));
---
+++ src/main/resources/db/migration/V2__guests.sql
@@ -0,0 +1,5 @@
+CREATE TABLE guests (
+id bigint(10) AUTO_INCREMENT,
+name varchar(255),
+room varchar(255),
+PRIMARY KEY (id));
